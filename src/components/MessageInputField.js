d383c60dc48af24dd1ed4b29394f618f97911d39
kubota <polykkpodh1243@gmail.com>
2021-02-07 10:47:41 +0900

    generate avatar

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -2,19 +2,24 @@ import React from 'react';
 import { Avatar, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
+import { gravatarPath } from '../gravatar';
+
 const useStyles = makeStyles({
   root: {
     gridRow: 2,
+    margin: '26px',
   },
 });
 
-const MessageInputField = () => {
+const MessageInputField = ({ name }) => {
   const classes = useStyles();
+  const avatarPath  = gravatarPath(name);
+
   return (
     <div className={classes.root}>
       <Grid container>
         <Grid xs={1}>
-          <Avatar />
+          <Avatar src={avatarPath()}/>
         </Grid>
         <Grid xs={10}>入力</Grid>
         <Grid xs={1}>
